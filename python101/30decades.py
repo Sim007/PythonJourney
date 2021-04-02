347a05750a63f396c1f540f45d01ed946c042f58
sim007 <j.sim18011963@gmail.com>
2021-04-02 23:16:53 +0200

    new decades

--- python101/30decades.py
+++ python101/30decades.py
@@ -1,1 +1,8 @@
-# comment
+# decades.py
+
+age = int(input("How old are you?\n"))
+
+decades = age // 10
+years = age % 10
+
+print("You are " + str(decades) + " decades and "+str(years)+" years old")
