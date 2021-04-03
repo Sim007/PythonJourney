c62b574809a040cc974bbf354e620e6593eb498a
sim007 <j.sim18011963@gmail.com>
2021-04-03 20:20:58 +0200

    new ML

---
+++ ML101/20Deviation.py
@@ -0,0 +1,38 @@
+# Deviation
+
+# standard deviation
+
+import numpy
+
+speed = [86,87,88,86,87,85,86]
+
+x = numpy.std(speed)
+print(x)
+
+# standaard deviation 2
+import numpy
+
+speed = [32,111,138,28,59,77,97]
+
+x = numpy.std(speed)
+print(x)
+
+# Variance
+
+import numpy
+
+speed = [32,111,138,28,59,77,97]
+
+x = numpy.var(speed)
+
+print(x)
+
+# square (variance) = deviation
+
+import numpy
+
+speed = [32,111,138,28,59,77,97]
+
+x = numpy.std(speed)
+
+print(x)
