ece88f8ce4e747d6eda51aae279e636e66045c11
Johannes Sim <Sim007@users.noreply.github.com>
2021-10-18 13:33:01 +0200

    Dumb revert after wrong commit

--- my1ML/loadDataSet.py
+++ my1ML/loadDataSet.py
@@ -1,41 +1,11 @@
 #!/usr/bin/env python3
-# script summarizeData
-print("*** Script summarizeData.py ***")
-
+# script loadDataSet
 
 # summarize the data
 from pandas import read_csv
 
 # Load dataset
 # url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
-url = https://raw.githubusercontent.com/Sim007/PythonJourney/main/my1ML/summarizeData.py
+url = https://raw.githubusercontent.com/Sim007/PythonJourney/main/my1ML/iris.csv
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
-
-# shape
-print("*** Shape ***)
-print()
-print(dataset.shape)
-print()
-print()
-
-# head
-print("*** Head ***)
-print()
-print(dataset.head(20))
-print()
-print()
-
-# descriptions
-print("*** Discription ***)
-print()
-print(dataset.describe())
-print()
-print()
-
-# class distribution
-print("*** Class distribution ***)
-print()
-print(dataset.groupby('class').size())
-print()
-print()
